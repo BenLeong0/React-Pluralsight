bc9073245ce90820952dcbcdb866c2ab83766cd1
= <benleong0@gmail.com>
2021-01-29 12:37:10 +0000

    2-3

--- rgs2_githubCardsFunctions.js
+++ rgs2_githubCardsFunctions.js
@@ -1,10 +1,12 @@
-const CardList = (props) => (
-	<div>
-  	{props.profiles.map(profile => <Card key={profile.id} {...profile}/>)}
-	</div>
+const CardList = props => (
+  <div>
+    {props.profiles.map(profile => (
+      <Card key={profile.id} {...profile} />
+    ))}
+  </div>
 );
 
-const Card = (props) => (
+const Card = props => (
   <div className="github-profile">
     <img src={props.avatar_url} />
     <div className="info">
@@ -12,45 +14,44 @@ const Card = (props) => (
       <div className="company">{props.company}</div>
     </div>
   </div>
-)
+);
 
-const Form = (props) => {
-	const [userName, setUserName] = useState('');
-	const handleSubmit = async (event) => {
-  	event.preventDefault();
+const Form = props => {
+  const [userName, setUserName] = useState("");
+  const handleSubmit = async event => {
+    event.preventDefault();
     const resp = await axios.get(`https://api.github.com/users/${userName}`);
-      // using ` => template string
+    // using ` => template string
     props.onSubmit(resp.data);
-    console.log(userName)
-		setUserName('')
+    console.log(userName);
+    setUserName("");
   };
-	return (
-		<form onSubmit={handleSubmit}>
-			<input
-				type="text"
-				value={userName}
-				onChange={event => setUserName(event.target.value)}
-				placeholder="GitHub username"
-				required
-			/>
-			<button>Add card</button>
-		</form>
-	);
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={userName}
+        onChange={event => setUserName(event.target.value)}
+        placeholder="GitHub username"
+        required
+      />
+      <button>Add card</button>
+    </form>
+  );
+};
 
-const App = (props) => {
-  const [profiles, setProfiles] = useState([])
-  const addNewProfile = (profileData) => {setProfiles([...profiles, profileData])}
+const App = props => {
+  const [profiles, setProfiles] = useState([]);
+  const addNewProfile = profileData => {
+    setProfiles([...profiles, profileData]);
+  };
   return (
     <div>
       <div className="header">{props.title}</div>
-      <Form onSubmit={addNewProfile}/>
+      <Form onSubmit={addNewProfile} />
       <CardList profiles={profiles} />
     </div>
   );
 };
 
-ReactDOM.render(
-	<App title="The GitHub Cards App" />,
-  mountNode,
-);
+ReactDOM.render(<App title="The GitHub Cards App" />, mountNode);
